e92e0ebce2fd849be99d24ed6b590db07e5ecbaf
Formacion <Formacion@host.docker.internal>
2020-06-03 17:02:53 +0200

    primer commit

---
+++ src/main/java/com/mercadoSegundaMano/MercadoSengundaManoApplication.java
@@ -0,0 +1,13 @@
+package com.mercadoSegundaMano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoSengundaManoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoSengundaManoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mercadoSegundaMano/MercadoSengundaManoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadoSegundaMano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoSengundaManoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
